07c76bc018343208b0af67f1ac1b34858f12735c
João Pedro <jpedrodss@gmail.com>
2020-05-29 09:41:16 -0300

    projeto criado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula06Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
